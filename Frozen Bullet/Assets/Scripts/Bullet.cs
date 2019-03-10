6d1eace56a56457cbf5c6ed39485c6d4e00db299
Alan Eir <QwaserLi@users.noreply.github.com>
2019-03-10 22:41:41 +1300

    Added basic enemy and shooting

---
+++ Frozen Bullet/Assets/Scripts/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    int damage = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 move = new Vector3(1, 0, 0);
+        transform.position += move * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(damage);
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //Do damage and destroy
+            Enemy e = collision.gameObject.GetComponent<Enemy>();
+            e.takeDamage(damage);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(damage);
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //Do damage and destroy
+            Enemy e = collision.gameObject.GetComponent<Enemy>();
+            e.takeDamage(damage);
+        }
+    }
+}
---
+++ Frozen Bullet/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject bullet;
+    public float speed;
+    public int health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+        if (Input.GetKey(KeyCode.Space)) {
+            Fire();
+        }
+    }
+
+
+    void movement() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+        transform.position += (speed * movement * Time.deltaTime);
+    }
+
+    void Fire() {
+        Instantiate(bullet, transform.position, transform.rotation);
+    }
+}
